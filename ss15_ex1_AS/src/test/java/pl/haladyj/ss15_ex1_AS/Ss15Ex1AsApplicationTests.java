eb7597d4aeae27c0bacade481d5cf6775f96336c
Paweł Haładyj <pawel@haladyj.pl>
2020-05-30 20:14:35 +0200

    initial commit

---
+++ ss15_ex1_AS/src/test/java/pl/haladyj/ss15_ex1_AS/Ss15Ex1AsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.ss15_ex1_AS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss15Ex1AsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
